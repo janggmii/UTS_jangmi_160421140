cb229b9575cfcb9f90b97fb70db65c2fe5953faa
janggmii <janggmii17@gmail.com>
2024-04-21 21:34:02 +0700

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "UTS_Jangmi_160421140"
+include(":app")
+ 
